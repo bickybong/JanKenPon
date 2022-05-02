39fe60010a7a7d2bf29160f352c2cd54e8537dcf
Zephan <bickybong@gmail.com>
2022-05-02 09:57:11 +0700

    add disable button after  game ends and bugfix for scissor score not updating

--- js.js
+++ js.js
@@ -26,11 +26,29 @@ function round(playerSelection, computerselection){
         if (computerselection === "Paper"){
             return  alert("You win! Scissor beats Paper"), playerScore++, comScore
         } else if (computerselection === "Scissor"){
-            return alert("Its a Scissor tie"), playerScore, comScore++
+            return alert("Its a Scissor tie"), playerScore, comScore
         } else{
-            return alert("You lose! Rock beats Scissor"), playerScore, comScore
+            return alert("You lose! Rock beats Scissor"), playerScore, comScore++
         }}
     }}
+
+function disableButtons() {
+    btn.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function checkScore(){
+    score.textContent =`Player score is ${playerScore}, Com score is ${comScore}`;
+    if (playerScore >=5){
+        alert("YOU WIN KING");
+        winOrLose.textContent = "YOU WIN KING";
+        disableButtons();
+    } else if (comScore >=5){
+        alert("SCRUB YOU LOSE");
+        winOrLose.textContent = "SCRUB YOU LOSE";
+        disableButtons();
+    }
 
 const rock = document.querySelector('#rock'); 
 rock.addEventListener('click', () => 
@@ -44,23 +62,9 @@ const scissor = document.querySelector('#scissor');
 scissor.addEventListener('click', () => 
  round("scissor", compueterPlay(),playerScore,comScore));
 
-function checkScore(){
-    score.textContent =`Player score is ${playerScore}, Com score is ${comScore}`;
-    if (playerScore >=5){
-        alert("YOU WIN KING");
-        winOrLose.textContent = "YOU WIN KING";
-    } else if (comScore >=5){
-        alert("SCRUB YOU LOSE");
-        winOrLose.textContent = "SCRUB YOU LOSE";
-    }
-}
-
 const score = document.querySelector('#score'); 
 const winOrLose = document.querySelector('#winOrLose');
 winOrLose.textContent = "LETS GOOOOOOOO";
-
-
-
 
 let playerScore = 0, comScore = 0;
 score.textContent = `Player score is ${playerScore}, Com score is ${comScore}`;
@@ -69,28 +73,4 @@ const btn = document.querySelectorAll('button');
 btn.forEach((button)=>{
     button.addEventListener('click', checkScore);
 })
-
-
-
-
-function game5(){
-    let score = 0
-    for (let i=0; i<5; i++){
-        let player = prompt("Choose rock, paper or scissor")
-        let result = round(player, compueterPlay());
-        if (result.includes("win")){
-            score++
-        } else if (result.includes("lose")){
-            score--
-        }
-        alert(`${result}, Current score is ${score}`)
-    }
-    if (score>0){
-        alert(`You  Win! Final score is ${score}!`)
-    } else if (score<0) {
-        alert(`You Lose! Final score is ${score}!`)
-    } else{
-        alert(`You Tie! Final score is ${score}!`)
-    }
-}
 
